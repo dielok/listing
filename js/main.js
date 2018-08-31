09aefd4e912917f2e2d0602c47d8d4bb6d3f0f64
Kevin Heusinger <kh@element1media.com>
2018-08-31 14:56:34 +0200

    final refactoring

--- js/main.js
+++ js/main.js
@@ -1,99 +1,96 @@
 "use strict";
 
-function loadDataPage(start, offset, callback) {
+(function ListingController() {
     
-    busy = true;
-    $listing.addClass('loading');
-    
-    /* TODO: DO LOADING STUFF AND CALLBACK WHEN DONE
-    $.get("products.json", function(data) {});
-    */
-    
-    setTimeout(function(){ callback(data.slice(0, offset)); }, 3000);
-    
-}
-
-function renderPage(data) {
-    
-    for (let i = 0; i < data.length; i++) {
+    function loadDataPage(start, offset, callback) {
         
-        $clone = $original.clone().appendTo($listing);
+        busy = true;
+        $listing.addClass('loading');
+        
+        /* TODO: DO LOADING STUFF AND CALLBACK WHEN DONE
+        $.get("products.json", function(data) {});
+        */
+        
+        setTimeout(function(){ callback(data.slice(0, offset)); }, 3000); // latency simulated for presentation
         
     }
     
-    loadPosition = $listing.offset().top + $listing.outerHeight() - 1.2 * $(window).height();
-    
-    busy = false;
-    $listing.removeClass('loading');
-    
-}
-
-function testMaxItems() {
-    
-    var maxPerPage = 0;
-    
-    do {
+    function renderPage(data) {
         
-        $clone = $original.clone().appendTo($listing);
-        
-        maxPerPage++;
-        
-    }
-    while ($clone.visible(true) && maxPerPage < 60);
-    
-    $('.product', $listing).remove();
-    $listing.removeClass('testing');
-    
-    if (maxPerPage < 10) maxPerPage = 10;
-    
-    return maxPerPage;
-    
-}
-
-
-var $listing, $clone, $original;
-
-$listing = $('.listing');
-$original = $('.product');
-
-var data = products.data, maxPerPage = testMaxItems(), busy = true, loadPosition;
-
-loadDataPage(0, maxPerPage * 2, function(page) {
-        
-    $(document).on('scroll', function() {
-        
-        var scrollPosition = $(this).scrollTop() + $(window).height();
-        
-        if (scrollPosition >= loadPosition) { // reached end of list
+        for (let i = 0; i < data.length; i++) {
             
-            if ( ! busy) {
-                
-                loadDataPage($('.product', $listing).length, maxPerPage, renderPage);
-                
-            }
+            $clone = $original.clone().appendTo($listing);
             
         }
         
+        loadPosition = $listing.offset().top + $listing.outerHeight() - 1.2 * $(window).height();
+        
+        busy = false;
+        $listing.removeClass('loading');
+        
+    }
+    
+    function testMaxItems() {
+        
+        var maxPerPage = 0;
+        
+        do {
+            
+            $clone = $original.clone().appendTo($listing);
+            
+            maxPerPage++;
+            
+        }
+        while ($clone.visible(true) && maxPerPage < 60);
+        
+        $('.product', $listing).remove();
+        $listing.removeClass('testing');
+        
+        if (maxPerPage < 10) maxPerPage = 10;
+        
+        return maxPerPage;
+        
+    }
+    
+    var $listing, $clone, $original;
+    
+    $listing = $('.listing');
+    $original = $('.product');
+    
+    var data = products.data, maxPerPage = testMaxItems(), busy = true, loadPosition;
+    
+    loadDataPage(0, maxPerPage * 2, function(page) {
+        
+        $(document).on('scroll', function() {
+            
+            var scrollPosition = $(this).scrollTop() + $(window).height();
+            
+            if (scrollPosition >= loadPosition) { // reached end of list
+                
+                if ( ! busy) {
+                    
+                    loadDataPage($('.product', $listing).length, maxPerPage, renderPage);
+                    
+                }
+                
+            }
+            
+        });
+        
+        renderPage(page);
+        
     });
     
-    renderPage(page);
+    $(window).on("resize", function(e) {
+        
+        loadPosition = $listing.offset().top + $listing.outerHeight() - 1.2 * $(this).height();
+        
+    });
     
-});
-
-$(window).on("resize", function(e) {
+    $('.sidebar__filter').on('click', function() {
+        
+        $(this).toggleClass("sidebar__filter--open");
+        
+    });
     
-    loadPosition = $listing.offset().top + $listing.outerHeight() - 1.2 * $(this).height();
-    
-});
-
-
-
-/////
-
-
-
-$('.sidebar__filter').on('click', function() {
-    
-    $(this).toggleClass("sidebar__filter--open");
-    
-});
+})();
